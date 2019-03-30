ef5c7f7f6e7df100e12b21fab7a37b151a64ad61
Chris Luettke <chrisluet@gmail.com>
2019-03-30 14:50:18 -0400

    Changing name of my first test for clarity.

--- Checkout/CheckoutTest.cpp
+++ Checkout/CheckoutTest.cpp
@@ -1,7 +1,8 @@
 #include "Checkout.h"
 #include "gtest\gtest.h"
 
-TEST(CheckoutTest, WhenSoupIsScannedPriceOfSoupIsAddedToTotal) {
+TEST(CheckoutTest, WhenGetPriceOfItemIsCalledForACanOfSoupItReturnsTheRegularPrice) {
 	Checkout checkout;
 	EXPECT_DOUBLE_EQ(1.89, checkout.GetPriceOfItem("soup"));
 }
+
